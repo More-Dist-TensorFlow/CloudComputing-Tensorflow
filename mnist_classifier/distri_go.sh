41ed1c95213eff0e31e8e2fa6859e880e178cfdd
Recolic Keghart <root@recolic.net>
2018-02-27 23:51:20 +0800

    quick_fix

--- mnist_classifier/distri_go.sh
+++ mnist_classifier/distri_go.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/sh
 
 # Now set env like:
-# export TF_CONFIG='{"ps":["ps1.example.com:2222","ps2.example.com:2222"],"worker":["worker1.example.com:2222"]}'
+# export TF_CONFIG='{"ps":["ps1.example.com:2222","ps2.example.com:2222"],"worker":["worker1.example.com:2222"],"task":{"type":"ps","index":0}}'
 
 # Parameter Server 1:
 
-python tfconfig_wrapper.py --job_name=ps --task_index=0  --data_dir=./data/
+python tfconfig_wrapper.py --data_dir=./data/
 
 # Parameter Server 2:
 
-python tfconfig_wrapper.py --job_name=ps --task_index=1 --ps_hosts=this_arg_is_ignored_now --worker_hosts=and_so_do_this_one --data_dir=./data/
+python tfconfig_wrapper.py  --data_dir=./data/
 
 
 # Worker 1
 
-python tfconfig_wrapper.py --job_name=worker --task_index=0 --data_dir=./data/
+python tfconfig_wrapper.py  --data_dir=./data/
 
 # Worker 2
 
-python tfconfig_wrapper.py --job_name=worker --task_index=1  --data_dir=./data/
+python tfconfig_wrapper.py --data_dir=./data/
 
